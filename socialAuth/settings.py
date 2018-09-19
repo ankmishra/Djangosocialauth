c1b0e16a7fc2fc54e54a392fed5f0ba54c76ce59
Ankit Mishra <ankit.mishra@fullthrottlelabs.com>
2018-09-19 15:08:00 +0530

    fixed the error

--- socialAuth/settings.py
+++ socialAuth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l&0b)3r^9n$!%*gl@k=&x7$!_w#6w4*^zj6^$e2gjzf^_25&ai'
+#SECRET_KEY = 'l&0b)3r^9n$!%*gl@k=&x7$!_w#6w4*^zj6^$e2gjzf^_25&ai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,11 +86,12 @@ WSGI_APPLICATION = 'socialAuth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
@@ -136,4 +137,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- socialAuth/wsgi.py
+++ socialAuth/wsgi.py
@@ -1,9 +1,8 @@
-# wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heroku_blog.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialAuth.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
